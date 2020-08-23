f6497321ac854319a9b03d898410d5b7825ec455
Diffloth, Antoine (ad7073) <adiffloth@gmail.com>
2020-08-23 17:16:52 -0500

    Complete problem 1

--- projects/Project 3/explanation_1.md
+++ projects/Project 3/explanation_1.md
@@ -1,2 +1,7 @@
 ## Square Root of an Integer
 
+We're looking for the largest number that when squared, results in a number that is not bigger than the input number.
+
+We could naively start at 1, square it, compare to the input and increment until we exceed or equal the input number. This would be an O(n) algorithm, but we can do better with a binary search.
+
+To implement the binary search, we define the search range
--- projects/Project 3/problem_1.py
+++ projects/Project 3/problem_1.py
@@ -1,6 +1,3 @@
-import math
-
-
 def sqrt(number):
     """
     Calculate the floored square root of a number
@@ -10,7 +7,29 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
     """
-    return math.sqrt(number)
+
+    if number is None:
+        return None
+
+    if number < 0:
+        return -1
+
+    if number == 0:
+        return 0
+
+    lower = 1
+    upper = number
+
+    while lower <= upper:
+        midpt_guess = (lower + upper) // 2
+        square = midpt_guess ** 2
+        plus_one_square = (midpt_guess + 1) ** 2
+        if square <= number < plus_one_square:  # Current guess is the sqrt, or incrementing guess goes too far.
+            return midpt_guess
+        elif square > number:  # Current guess is too high, search in the lower half.
+            upper = midpt_guess
+        else:  # Current guess is too low, search in the upper half.
+            lower = midpt_guess
 
 
 # Tests
@@ -20,26 +39,21 @@ assert 4 == sqrt(16)
 assert 1 == sqrt(1)
 assert 5 == sqrt(27)
 
+assert sqrt(None) is None  # Test None
+assert sqrt(-9) == -1  # Negative
+assert sqrt(-8474) == -1  # Big negative
+assert sqrt(0) == 0  # Base case
+assert sqrt(1) == 1  # The other base case
+assert sqrt(2) == 1  # Test floor
+assert sqrt(3) == 1
+assert sqrt(4) == 2  # Breakpoint
+assert sqrt(5) == 2  # Test floor again
+assert sqrt(6) == 2
+assert sqrt(7) == 2
+assert sqrt(8) == 2
+assert sqrt(9) == 3  # Breakpoint
+assert sqrt(81) == 9
+assert sqrt(1764) == 42
+assert sqrt(7142726233651801) == 84514651  # Large number
 
-# print("Pass" if (None == sqrt(None)) else "Fail")#Null input
-
-print("Pass" if (-1 == sqrt(-27)) else "Fail")  # Negative input test
-
-print("Pass" if (0 == sqrt(0)) else "Fail")  # Zero input
-print("Pass" if (1 == sqrt(1)) else "Fail")  # 1 input
-print("Pass" if (1 == sqrt(2)) else "Fail")  # 2 input
-print("Pass" if (1 == sqrt(3)) else "Fail")  # 3 input
-print("Pass" if (2 == sqrt(4)) else "Fail")  # Negative input
-
-print("Pass" if (4 == sqrt(16)) else "Fail")
-print("Pass" if (3 == sqrt(15)) else "Fail")
-
-print("Pass" if (3 == sqrt(9)) else "Fail")
-
-print("Pass" if (4 == sqrt(16)) else "Fail")
-
-print("Pass" if (24 == sqrt(624)) else "Fail")
-print("Pass" if (31 == sqrt(1000)) else "Fail")
-
-# Large Number input
-print("Pass" if (166564950 == sqrt(27743882883774747)) else "Fail")
+print('Tests passed.')
