7f4aafd4d9b70ef4b6fd2fd492940cd8a00de648
Diffloth, Antoine (ad7073) <adiffloth@gmail.com>
2020-08-24 06:27:09 -0500

    Add recursive function to problem 1

--- projects/Project 3/problem_1.py
+++ projects/Project 3/problem_1.py
@@ -32,6 +32,21 @@ def sqrt(number):
             lower = midpt_guess
 
 
+def binary_search_recursive_soln(array, target, start_index, end_index):
+    if start_index > end_index:
+        return -1
+
+    mid_index = (start_index + end_index) // 2
+    mid_element = array[mid_index]
+
+    if mid_element == target:
+        return mid_index
+    elif target < mid_element:
+        return binary_search_recursive_soln(array, target, start_index, mid_index - 1)
+    else:
+        return binary_search_recursive_soln(array, target, mid_index + 1, end_index)
+
+
 # Tests
 assert 3 == sqrt(9)
 assert 0 == sqrt(0)
