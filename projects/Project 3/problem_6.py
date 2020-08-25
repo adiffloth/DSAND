87b8bfd4c2eba713a152b70b067e4bbcbd4ac919
Diffloth, Antoine (ad7073) <adiffloth@gmail.com>
2020-08-25 11:25:51 -0500

    Solve problem 6

--- projects/Project 3/problem_6.py
+++ projects/Project 3/problem_6.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 
 def get_min_max(ints):
@@ -8,12 +9,38 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
-    pass
+
+    if len(ints) == 0:
+        return (None, None)
+
+    min_num = math.inf
+    max_num = -math.inf
+
+    for num in ints:
+        max_num = max(num, max_num)
+        min_num = min(num, min_num)
+
+    return (min_num, max_num)
 
 
 # Example Test Case of Ten Integers
 
 ls = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(ls)
-
 print("Pass" if ((0, 9) == get_min_max(ls)) else "Fail")
+
+ls = [i for i in range(-9, 1)]  # a list containing -9 to 0
+random.shuffle(ls)
+print("Pass" if ((-9, 0) == get_min_max(ls)) else "Fail")
+
+ls = [i for i in range(-12, 13)]  # a list spanning zero
+random.shuffle(ls)
+print("Pass" if ((-12, 12) == get_min_max(ls)) else "Fail")
+
+ls = [42]  # a list with a single value
+random.shuffle(ls)
+print("Pass" if ((42, 42) == get_min_max(ls)) else "Fail")
+
+ls = []  # an empty list
+random.shuffle(ls)
+print("Pass" if ((None, None) == get_min_max(ls)) else "Fail")
