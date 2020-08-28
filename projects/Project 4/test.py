e6e9ab8080ffb000773965bdfff56d044f699086
Diffloth, Antoine (ad7073) <adiffloth@gmail.com>
2020-08-28 08:26:42 -0500

    Reset project

--- projects/Project 4/test.py
+++ projects/Project 4/test.py
@@ -2,10 +2,9 @@ from helpers import load_map
 
 MAP_40_ANSWERS = [
     (5, 34, [5, 16, 37, 12, 34]),
-    (5, 5, [5]),
+    (5, 5,  [5]),
     (8, 24, [8, 14, 16, 37, 12, 17, 10, 24])
 ]
-
 
 def test(shortest_path_function):
     map_40 = load_map('map-40.pickle')
@@ -15,7 +14,7 @@ def test(shortest_path_function):
         if path == answer_path:
             correct += 1
         else:
-            print("For start:", start,
+            print("For start:", start, 
                   "Goal:     ", goal,
                   "Your path:", path,
                   "Correct:  ", answer_path)
@@ -23,3 +22,4 @@ def test(shortest_path_function):
         print("All tests pass! Congratulations!")
     else:
         print("You passed", correct, "/", len(MAP_40_ANSWERS), "test cases")
+    
